ca24e772d5b678cca004db6a045a3567d6518a2a
Jesse Z <yzhang1005@gmail.com>
2023-10-23 14:42:24 -0500

    service poller

--- service/poll/poller.py
+++ service/poll/poller.py
@@ -11,7 +11,17 @@ django.setup()
 
 # Import models from service_rest, here. Ignore vs-code error hinting
 # from service_rest.models import Something
+from service_rest.models import AutomobileVO
 
+def get_automobiles():
+    response = requests.get("http://project-beta-inventory-api-1:8000/api/automobiles/")
+    content = json.loads(response.content)
+    print(content)
+    for automobile in content["automobiles"]:
+        AutomobileVO.objects.update_or_create(
+            import_href=automobile["href"],
+            defaults={"vin": bin["vin"]},
+        )
 
 def poll(repeat=True):
     while True:
@@ -19,8 +29,8 @@ def poll(repeat=True):
         try:
             # Write your polling logic, here
             # Do not copy entire file
-            pass
-        
+            get_automobiles()
+
         except Exception as e:
             print(e, file=sys.stderr)
 
