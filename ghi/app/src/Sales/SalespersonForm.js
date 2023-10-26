f9154c96482a0607c987216c6b95c1c2a7f36543
Hamza <hamzasahmed11@gitlab.com>
2023-10-26 09:57:03 -0400

    fixed salesperson history

--- ghi/app/src/Sales/SalespersonForm.js
+++ ghi/app/src/Sales/SalespersonForm.js
@@ -5,6 +5,71 @@ function SalespersonForm() {
   const [lastName, setLastName] = useState('')
   const [employeeId, setEmployeeId] = useState('')
 
+  const handleFirstNameChange = event => {
+    const value = event.target.value
+    setFirstName(value)
+  }
+
+  const handleLastNameChange = event => {
+    const value = event.target.value
+    setLastName(value)
+  }
+
+  const handleEmployeeIdChange = event => {
+    const value = event.target.value
+    setEmployeeId(value)
+  }
+
+  const handleSubmit = async (event) => {
+    event.preventDefault()
+    const data = {}
+    data.first_name = firstName
+    data.last_name = lastName
+    data.employee_id = employeeId
+
+    const salesPersonUrl = 'http://localhost:8090/api/salespeople/'
+    const fetchConfig = {
+      method: 'post',
+      body: JSON.stringify(data),
+      headers: {
+        'Content-Type': 'application/json'
+      }
+    }
+
+    const response = await fetch(salesPersonUrl, fetchConfig)
+    if (response.ok){
+      setFirstName('')
+      setLastName('')
+      setEmployeeId('')
+    }
+  }
+
+return (
+  <div className='row'>
+    <div className='offset-3 col 6'>
+      <div className='shadow p-4 mt-4'>
+        <h1>Add a Salesperson</h1>
+        <form onSubmit={handleSubmit}>
+          <div className='form-floating mb-3'>
+            <input value={firstName} onChange={handleFirstNameChange} placeholder='First Name' required type='text' id='first_name' name='first_name' className='form-control'/>
+            <label>First Name</label>
+          </div>
+          <div className='form-floating mb-3'>
+            <input value={lastName} onChange={handleLastNameChange} placeholder='Last Name' required type='text' id='last_name' name='last_name' className='form-control'/>
+            <label>Last Name</label>
+          </div>
+          <div className='form-floating mb-3'>
+            <input value={employeeId} onChange={handleEmployeeIdChange} placeholder='EmployeeId' type='number' id='employee_Id' name='employee_Id' className='form-control'/>
+            <label>Employee ID</label>
+          </div>
+          <button className='btn btn-primary'>Create</button>
+        </form>
+      </div>
+    </div>
+
+  </div>
+)
+
 
 }
 
--- ghi/app/src/Sales/SalespersonHistory.js
+++ ghi/app/src/Sales/SalespersonHistory.js
@@ -1,7 +1,96 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 
 function SalespersonHistory(){
+  const [sales, setSales] = useState([])
+  const [salespersons, setSalesperson] = useState([]);
+  const [salesPerson, setSalesPerson] = useState('');
 
+
+  async function loadSales() {
+    const response = await fetch('http://localhost:8090/api/sales/')
+    if (response.ok){
+      const data = await response.json()
+      setSales(data.sales)
+    }
+  }
+
+  async function loadSalespersons() {
+    const response = await fetch('http://localhost:8090/api/salespeople/');
+    if (response.ok) {
+      const data = await response.json();
+      setSalesperson(data.salespeople)
+    }
+  }
+
+  const filteredSales = sales.filter(
+    (sale) => sale.sales_person.employee_id === salesPerson
+  );
+
+  const handleSalesPersonChange = (event) => {
+    const value = event.target.value;
+    setSalesPerson(value);
+  }
+
+  useEffect(() => {
+    loadSales()
+    loadSalespersons()
+  }, [])
+
+  const handleSubmit = async (event) => {
+    event.preventDefault()
+    const data = {}
+    data.sales_person = salesPerson
+    const salesUrl = 'http://localhost:8090/api/sales/'
+    const fetchConfig = {
+      method: 'post',
+      body: JSON.stringify(data),
+      headers: {
+        'Content-Type': 'application/json',
+      }
+    }
+    const sale = await fetch(salesUrl, fetchConfig)
+    if (sale.ok) {
+      setSalesperson('');
+    }
+  }
+
+  return (
+    <>
+    <h1>Salesperson History</h1>
+    <div className="form-floating mb-3">
+      <select onChange={handleSalesPersonChange} placeholder="sales_person" type="text" name="sales_person"  id="sales_person" className="form-control">
+        <option value="">Choose a Salesperson</option>
+        {salespersons?.map(salespeople => {
+          return (
+          <option key={salespeople.id} value={salespeople.employee_id}>{salespeople.first_name} {salespeople.last_name}</option>)
+          })
+          }
+          </select>
+    </div>
+    <table className="table table-striped">
+      <thead>
+        <tr>
+          <th>Salesperson</th>
+          <th>Customer</th>
+          <th>VIN</th>
+          <th>Price</th>
+        </tr>
+      </thead>
+      <tbody>
+        {filteredSales.map(sale => {
+          return (
+            <tr key={sale.id}>
+              <td>{ sale.sales_person.first_name } { sale.sales_person.last_name }</td>
+                <td>{ sale.customer.first_name } { sale.customer.last_name }</td>
+                <td>{ sale.automobile.vin }</td>
+                <td>${sale.price}</td>
+            </tr>
+          )
+        })}
+      </tbody>
+    </table>
+    </>
+  )
 }
 
 export default SalespersonHistory
